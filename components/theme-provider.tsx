9a49171b8a090efcfc80494c5cbcfa16664722df
karman07 <karmansingharora01@gmail.com>
2026-01-03 21:36:10 +0530

    done

--- components/theme-provider.tsx
+++ components/theme-provider.tsx
@@ -5,7 +5,7 @@ import { ReactNode } from 'react'
 
 interface ThemeProviderProps {
   children: ReactNode
-  attribute?: string
+  attribute?: 'class' | 'data-theme' | string | string[]
   defaultTheme?: string
   enableSystem?: boolean
   disableTransitionOnChange?: boolean
