c1845e2fd05a04989b38392a30e0aa12e56e8bbc
karman07 <karmansingharora01@gmail.com>
2026-01-23 13:37:09 +0530

    login of employer

--- src/controllers/googleAuthController.ts
+++ src/controllers/googleAuthController.ts
@@ -4,24 +4,12 @@ import User from '../models/User';
 import CandidateProfile from '../models/CandidateProfile';
 import Company from '../models/Company';
 import { generateTokens } from '../utils/jwt';
+import path from 'path';
 
 // Initialize Firebase Admin SDK
 if (!admin.apps.length) {
-  const serviceAccount = {
-    type: "service_account",
-    project_id: process.env.GOOGLE_PROJECT_ID,
-    private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
-    private_key: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
-    client_email: process.env.FIREBASE_CLIENT_EMAIL,
-    client_id: process.env.FIREBASE_CLIENT_ID,
-    auth_uri: "https://accounts.google.com/o/oauth2/auth",
-    token_uri: "https://oauth2.googleapis.com/token",
-    auth_provider_x509_cert_url: "https://www.googleapis.com/oauth2/v1/certs",
-    client_x509_cert_url: process.env.FIREBASE_CLIENT_CERT_URL
-  };
-  
   admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount as admin.ServiceAccount)
+    credential: admin.credential.cert(path.join(__dirname, '../../youtube-data-api-v3-468414-firebase-adminsdk-fbsvc-eaa81bc7ec.json'))
   });
 }
 
